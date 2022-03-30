9c9759d42b678c5c5ff1bf4712463e277cd9a319
PurpleDragon103 <jessica.gonzalezmarte@my.strayer.edu>
2022-03-30 08:03:32 +0000

    .

---
+++ 09.05.py
@@ -0,0 +1,29 @@
+a = []
+n = int(input("Enter Size: "))
+for i in range(n+2):
+    a.append([' '] * (n+2))
+
+# corners
+a[0][0] = '+'
+a[0][-1] = '+'
+a[n+1][0] = '+'
+a[n+1][-1] = '+'
+for i in range(1,n+2):
+    # border lines
+    a[0][i] = '-'
+    a[i][0] = '|'
+    a[n+1][i] = '-'
+    a[i][n+1] = '|'
+
+# inside
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if (i % 2) == 0 and (j % 2) != 0:
+            a[i][j] = '*'
+            a[j][i] = '*'
+
+# print the picture 
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=' ') 
+    print()
---
+++ 09.06.py
@@ -0,0 +1,29 @@
+# Create the array 
+a = []
+total = 0
+# Open the file and read the first line 
+tempsfile = open("/workspace/IFSC1202SU/09.06 CityTemps.txt", 'r') 
+temps = tempsfile.readline() 
+
+while temps != "":
+    y = temps.split(" ")
+    for i in range(1,len(y)):
+        y[i] = int(y[i]) 
+    a.append(y)
+    temps = tempsfile.readline()
+tempsfile.close()
+
+for i in range(len(a)):
+    for j in range(1,len(a[i])):
+        total += a[i][j]
+    avg = total / (j-1)
+    a[i].append(int(avg))
+    total = 0
+
+print("{:<8} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8} {:<}".format('City','Mo','Tu','We','Th','Fr','Sa','Su','Avg'))
+# Print Process Loop
+for i in range(len(a)):
+    print(a[i][0], end='   ')
+    for j in range(1,len(a[i])):
+        print(a[i][j], end='       ')
+    print()
